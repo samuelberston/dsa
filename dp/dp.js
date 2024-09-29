582c3b8da044dc643969679cce7aeb24d6b1a3e7
Samuel Berston <samuelberston@gmail.com>
2024-09-29 00:48:13 -0700

    dynamic programming dir/file

---
+++ dp/dp.js
@@ -0,0 +1,13 @@
+/*
+    Dynamic Programming
+    
+    - Longest Common Subsequence
+*/
+
+/*
+    Longest Common Subsequence
+    Given two strings, S1 and S2, the task is to find the length of 
+    the Longest Common Subsequence. If there is no common subsequence, return 0.
+
+    Geeks for geeks - https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+*/
