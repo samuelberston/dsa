4a5ff459a0f739b57662ec401cc3cb10e7ebce95
Samuel Berston <samuelberston@gmail.com>
2024-11-30 15:50:30 -0800

    backtracking - sudoku solver

--- backtracking/backtracking.js
+++ backtracking/backtracking.js
@@ -4,6 +4,8 @@
  * 
  *      1. Letter Combinations of a Phone Number
  *      2. Generate Combinations
+ *      3. Sudoku Solver
+ * 
  */
 
 /**
@@ -128,3 +130,119 @@ if (JSON.stringify(res2a) === JSON.stringify([
   } else {
     console.log("FAILED");
   }
+
+/**
+ *      3. Sudoku Solver
+ * 
+ *      Example:
+ *      Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+ *      Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+ * 
+ *      Approach: backtracking 
+ *      - iterate through each cell in the board 
+ *      - calculate possible solutions based on checker helper 
+ *      - recurse on possible solutions
+ *      - base case: reached last cell - check board
+*/
+const sudokuSolver = (board) => {
+
+    // initialize arrays to track visited rows, columns, boxes
+    const rows = Array(9).fill().map(() => ({}));
+    const columns = Array(9).fill().map(() => ({}));
+    const boxes = Array(9).fill().map(() => ({}));
+    let solved = false;
+
+    // box index helper
+    const boxIndex = (r, c) => Math.floor(r / 3) * 3 + Math.floor(c / 3);
+    
+
+    // could place helper
+    const couldPlace = (r, c, n) => {
+        return !rows[r][n] && !columns[c][n] && !boxes[boxIndex(r, c)][n]; 
+    }
+
+    // place number helper
+    const placeNumber = (r, c, n) => {
+        rows[r][n] = true;
+        columns[c][n] = true;
+        boxes[boxIndex(r, c)][n] = true;
+        board[r][c] = n;
+    }
+
+    // place next numbers
+    const placeNextNumbers = (r, c) => {
+        // last cell
+        if (c === 8 && r === 8) {
+            solved = true;
+        } else if (c === 8) {
+            backtrack(r + 1, 0);
+        } else {
+            backtrack(r, c + 1);
+        }
+    };
+
+    // remove number helper
+    const removeNumber = (r, c, n) => {
+        rows[r][n] = false;
+        columns[c][n] = false;
+        boxes[boxIndex(r, c)][n] = false;
+        board[r][c] = '.';
+    }
+
+    // backtrack
+    const backtrack = (r, c) => {
+        if (solved) return;
+
+        // base case
+        if (r === 9) { // last row
+            solved = true;
+            return;
+        }
+        if (c === 9) { // next row
+            backtrack(r + 1, 0);
+            return;
+        }
+
+        // empty cell
+        if (board[r][c] === '.') {
+            // iterate candidates
+            for (let n = 1; n < 10; n++) {
+                const strN = n.toString();
+                if (couldPlace(r, c, strN)) { 
+                    placeNumber(r, c, strN);
+                    backtrack(r, c + 1); // next cell
+                    // back track
+                    if (!solved) removeNumber(r, c, strN);
+
+                }
+            }
+        } else {
+            backtrack(r, c + 1); // next cell
+        }
+    }
+    // First pass - register all initial values
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] !== '.') {
+                placeNumber(r, c, board[r][c]);
+            }
+        }
+    }
+
+    // backtrack from first cell
+    backtrack(0, 0);
+    return board;
+}
+
+console.log("\n3. Sudoku Solver");
+process.stdout.write("TEST CASE 1: ");
+const board1 = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
+sudokuSolver(board1);
+const solution1 = [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]];
+console.log(JSON.stringify(board1) === JSON.stringify(solution1) ? "PASSED" : "FAILED");
+
+process.stdout.write("TEST CASE 2: ");
+const board2 = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]];
+sudokuSolver(board2);
+const solution2 = [["5","1","9","7","4","8","6","3","2"],["7","8","3","6","5","2","4","1","9"],["4","2","6","1","3","9","8","7","5"],["3","5","7","9","8","6","2","4","1"],["2","6","4","3","1","7","5","9","8"],["1","9","8","5","2","4","3","6","7"],["9","7","5","8","6","3","1","2","4"],["8","3","2","4","9","1","7","5","6"],["6","4","1","2","7","5","9","8","3"]];
+console.log(JSON.stringify(board2) === JSON.stringify(solution2) ? "PASSED" : "FAILED");
